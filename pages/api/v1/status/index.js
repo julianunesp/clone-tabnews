12a9c800f7f5f0cd7c9177479443b3553d2096c6
Xu <101535045+pedrosaxu@users.noreply.github.com>
2025-03-07 11:55:59 +0000

    alterando a chave e valor do endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
 function status(request, response) {
-  response.status(200).json({ chave: "são acima da média" });
+  response.status(200).json({ minhachave: "meu valor" });
 }
 
 export default status;
