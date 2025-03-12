9420f11e35d14c725fba1a2e00145af835a488a7
Xu <101535045+pedrosaxu@users.noreply.github.com>
2025-03-12 11:58:17 +0000

    make `database.js` more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,8 +9,16 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
+
   return result;
 }
 
