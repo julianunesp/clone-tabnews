3cda5d1380afb397fa9a4032999ed22e984c5d20
Xu <101535045+pedrosaxu@users.noreply.github.com>
2025-03-29 14:49:29 +0000

    fixing `postgress_version` validation tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,9 +9,11 @@ test("GET to /api/v1/status should return 200", async () => {
 
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.postgres_version).toEqual("16.0");
+  expect(responseBody.dependencies.database.postgres_version)
+    .toString()
+    .includes("16.");
 
-  expect(responseBody.dependencies.database.current_connections).toEqual(1);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
